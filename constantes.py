fc3585e6ebd740e45163ba48a5ae7cc6b791db0e
CarlosGitto <63372445+CarlosGitto@users.noreply.github.com>
2021-11-05 23:09:26 -0300

    Delete constantes.py

--- constantes.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-kn_lst=[1,2,3,4,5,6,7,8,9]
-kn_lst_2=np.array([["a","b","c"],["d","e","f"],["g","h","i"]])
-kn_lst_3=["a","b","c","d","e","f","g","h","i"]
-sudoku_compare=np.zeros((9,9),dtype=int)
