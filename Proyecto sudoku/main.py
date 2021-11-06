3a4e1063e6e622d20395705cdcd28062f7bae5f8
CarlosGitto <63372445+CarlosGitto@users.noreply.github.com>
2021-11-05 23:13:34 -0300

    Update main.py

--- Proyecto sudoku/main.py
+++ Proyecto sudoku/main.py
@@ -15,16 +15,12 @@ my_sudoku=np.array([[0,0,0, 2,0,9, 0,6,0],
 
                     [0,0,0, 3,8,6 ,0,7,0],
                     [0,0,1, 0,0,0, 3,0,2],
-                    [0,0,8, 0,2,0, 0,0,0]])             #sudoku[8]
-my_sudoku_trasp=(my_sudoku.transpose())
+                    [0,0,8, 0,2,0, 0,0,0]])            
+
 
                
-                    #print(val,"esta",count,"veces en la fila",i,"en las posiciones",l)
-dictionary(my_sudoku,wrk_pos)
-#################################################
-#           Sabe el estado del sudoku           #
-#                                               #
-#################################################
+                  
+dictionary(my_sudoku,wrk_pos) #inicia el diccionario
 
 
         
@@ -77,34 +73,6 @@ print(my_sudoku)
 
 
 
-#######################################################################
-##                                                                    #
-##                        Trabajo con diccionario                     #
-#######################################################################
-
-                
-            
-  # only_one_option_in_family(dict,sudoku)
-   # print(wrk_pos)
-       
-    #frm_dict_to_mtrx(wrk_pos,sudoku)
-
-
-"""  def frm_dict_to_mtrx(dict,sudoku):
-        
-        for i in range(9):
-            for j in range(9):
-                family=func_abc(dict,i,j)
-                num=str(i)+str(j)
-                full_name=family+num
-                if sudoku[i,j]==0 and type(dict[full_name])!=list:
-                    
-                    sudoku[i,j]=dict[full_name]
-                    print(full_name,"es",sudoku[i,j])
-                else:
-                    continue
-            
-        sudoku_complete(sudoku)"""
 
 
 
@@ -114,7 +82,5 @@ print(my_sudoku)
 
 
 
-"""Estructura que quiero tener:
-1 funcion que acomode el diccionario en primer lugar y defina los falores faciles,
-cuando no haya mas valores faciles, chequee que no este resuelto y luego vaya a los dificiles hasta resolverlo
-siemrpe tiene que  haber una funcion actualizando el dict y los valores resueltos(put_val)"""
+
+
